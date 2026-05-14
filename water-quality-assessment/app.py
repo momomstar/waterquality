222203c2ad808e0215314dfe3ac12d13c20dc3ba
momomstar <paripot4@gmail.com>
2026-05-14 15:48:53 +0700

    initial commit

--- water-quality-assessment/app.py
+++ water-quality-assessment/app.py
@@ -2479,9 +2479,7 @@ def internal_error(error):
 # =========================
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
     with app.app_context():
         db.create_all()
         check_and_sync_fuzzy()
-
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
